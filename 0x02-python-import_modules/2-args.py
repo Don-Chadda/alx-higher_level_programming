0b7199c02f0fa20eccb086d9195e7ddebcbb3cb0
Peter Daniel Chadda Turay <69143593+Don-Chadda@users.noreply.github.com>
2022-09-22 19:38:40 +0000

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    total = len(sys.argv)
-    if total <= 1:
+import sys
+
+
+def main():
+    argc = len(sys.argv) - 1
+    if argc == 0:
         print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
     else:
-        if total == 2:
-            print("{:d} argument:".format(total - 1))
-        else:
-            print("{:d} arguments:".format(total - 1))
-        for i in range(1, total):
-            print("{:d}: {}".format(i, sys.argv[i]))
+        print("{:d} arguments:".format(argc))
+        for i in range(1, argc + 1):
+            print("{}: {}".format(i, sys.argv[i]))
+
+if __name__ == "__main__":
+    main()
