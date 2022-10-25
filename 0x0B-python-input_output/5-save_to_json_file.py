d40991d4d9173057719e9f172ea729c9e4dc02cf
Peter Daniel Chadda Turay <69143593+Don-Chadda@users.noreply.github.com>
2022-10-25 11:17:13 -0700

    Update 5-save_to_json_file.py

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
+"""Module 7-save_to_json_file.
+Writes an Object to a text file,
+using a JSON representation.
 """
-Contains the "from_json_string" function
-"""
+
 
 import json
 
 
-def from_json_string(my_str):
-    """returns an object represented by a JSON string"""
-    return json.loads(my_str)
+def save_to_json_file(my_obj, filename):
+    """Writes the representation of my_obj
+    to filename.
+    Args:
+        - my_obj: object to write
+        - filename: file to write into
+    """
+
+    with open(filename, 'w+') as f:
+        json.dump(my_obj, f)
